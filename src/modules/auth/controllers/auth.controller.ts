23fd3d1a37610922e423e1dd3033a67396ddb76d
Anton Yastrebov <tohix.anton@gmail.com>
2026-07-01 22:46:39 +0300

    feat: added auth controller

---
+++ src/modules/auth/controllers/auth.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CurrentUser } from '../decorators/current-user.decorator';
+import { LoginDto } from '../dto/login.dto';
+import { RegisterDto } from '../dto/register.dto';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import type { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { AuthService } from '../services/auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  register(@Body() dto: RegisterDto) {
+    return this.authService.register(dto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  me(@CurrentUser() user: JwtPayload) {
+    return user;
+  }
+}
